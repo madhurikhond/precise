ee8d5527340b4ef61c96963e911dd9f834c0f176
sureshsensationsolutions <suresh.thakur@sensationsolutions.in>
2022-09-30 19:05:14 +0530

    Email regex correct

--- src/app/constants/commonregex.ts
+++ src/app/constants/commonregex.ts
@@ -1,5 +1,5 @@
 export const CommonRegex={    
-    EmailRegex:/^[A-Z[a-z0-9._%+-]+@[A-Za-z0-9.]+\.[A-Za-z]{2,6}$/  ,
+    EmailRegex:/^[A-Z[a-z0-9._-]+@[A-Za-z0-9.]+\.[A-Za-z]{2,6}$/  ,
     FaxRegex:/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/,
     PhoneRegex:/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/
 
