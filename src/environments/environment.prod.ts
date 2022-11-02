ee1af1dd6dfa6793399c8e7aaae29fe190c6c97c
chirag kamani <tbs.chiragk@gmail.com>
2022-11-02 12:58:42 +0530

    commit for change in production environment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   //baseUrl: `http://192.168.1.24:8554/api`,
   baseUrl: `/API/api`,
   //baseUrl: `http://localhost:81/api`,
-  basePatientUrl: `https://staging-precise.radflow360.com/PatientPortalAPI/`,
+  basePatientUrl: `/PatientPortalAPI/`,
   apiKey: 'f0M65v8av8ns3iZ4XFEacXc1dKWqWI6756Nb4nRVymYysN1jtKmSBQUyEfgGeRc3tDyBF5bP61Z8VcT4zm8GvCe8xSiLgS143V6Y3OQ4a062qutS13qgx55T4A9DNhAk',
   onlyDomain: ``,
   currentVersion: `1.0`,
