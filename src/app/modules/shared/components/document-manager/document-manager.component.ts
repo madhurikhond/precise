a51ce42b37e17f388434c4944ba2cabaae6d686b
Mridula <itsen@SCLIENT-53L>
2022-11-02 16:01:53 +0530

    R3-827

--- src/app/modules/shared/components/document-manager/document-manager.component.ts
+++ src/app/modules/shared/components/document-manager/document-manager.component.ts
@@ -334,10 +334,11 @@ export class DocumentManagerComponent implements OnInit, AfterViewInit {
 
       if (this.selectedFileKeys.length == 1) {
         this.downloadFile(this.selectedFileNames, this.selectedFileBase64String)
-        //this.getFilesByKey(this.selectedFileNames, this.path, e.itemData.text, e)
+        this.getFilesByKey(this.selectedFileNames, this.path, e.itemData.text, e)
         //this.clearSelectedFields();
       }
       else if (this.selectedFileKeys.length > 1) {
+       
         this.getFilesByKeys(e.itemData.text)
         //this.downloadAllFilesAsZipFile(this.selectedFileItems);
       }
@@ -346,7 +347,7 @@ export class DocumentManagerComponent implements OnInit, AfterViewInit {
       }
     }
     else if (e.itemData.text == 'Download All Files') {
-      this.getFilesByKeys(e.itemData.text)
+      //this.getFilesByKeys(e.itemData.text)
       //this.downloadAllFilesAsZipFile(this.fileItems);
     }
     else if (e.itemData.text == 'Upload') {
@@ -488,18 +489,18 @@ export class DocumentManagerComponent implements OnInit, AfterViewInit {
     if (this.selectedFileNames) {
       this.selectedFileKeys = e.selectedItemKeys;
       var fileExtension = this.selectedFileNames.split('.').pop();
-      this.documentmanagerService.getFilesByKey(true, JSON.stringify(e.selectedItems[0].dataItem.filePath)).subscribe((res) => {
-        if (res.response != null) {
-          console.log()
-          this.path = JSON.parse(res.response).Base64;
-        }
-      })
-      if (this.selectedFileNames.match(/.(jpg|jpeg|png|gif)$/i)) {
-        this.selectedFileBase64String = 'data:image/' + fileExtension + ';base64,' + this.path;
-      }
-      else if (this.selectedFileNames.match(/.(pdf)$/i)) {
-        this.selectedFileBase64String = 'data:application/pdf;base64,' + this.path;
-      }
+      // this.documentmanagerService.getFilesByKey(true, JSON.stringify(e.selectedItems[0].dataItem.filePath)).subscribe((res) => {
+      //   if (res.response != null) {
+      //     console.log()
+      //     this.path = JSON.parse(res.response).Base64;
+      //   }
+      // })
+      // if (this.selectedFileNames.match(/.(jpg|jpeg|png|gif)$/i)) {
+      //   this.selectedFileBase64String = 'data:image/' + fileExtension + ';base64,' + this.path;
+      // }
+      // else if (this.selectedFileNames.match(/.(pdf)$/i)) {
+      //   this.selectedFileBase64String = 'data:application/pdf;base64,' + this.path;
+      // }
 
 
     }
@@ -536,7 +537,8 @@ export class DocumentManagerComponent implements OnInit, AfterViewInit {
     link.click();
   }
   selectedFileDisplay(e) {
-    this.getFilesByKey(e.file.dataItem.name, e.file.dataItem.filePath, 'Open', e)
+    //this.getFilesByKey(e.file.dataItem.name, e.file.dataItem.filePath, 'Open', e)
+    this.displayFile(e.file.name, e.file.dataItem.filePath);
   }
   downloadFileFromUrl(fileName, fileData) {
     this.fileName = fileName;
@@ -796,7 +798,6 @@ export class DocumentManagerComponent implements OnInit, AfterViewInit {
     });
   }
   deleteOrCancelItem(isItemDelete: boolean) {
-
     if (isItemDelete) {
       if (this.selectedDeletedDoc.length > 0) {
         this.documentmanagerService.deleteAllFiles(true, this.selectedDeletedDoc).subscribe((res) => {
@@ -826,7 +827,7 @@ export class DocumentManagerComponent implements OnInit, AfterViewInit {
     }
   }
   renameOrCancelItem(isItemRename: boolean) {
-
+    debugger
     this.submitted = true;
     this.modelValue = 'modal';
     if (this.renameForm.invalid) {
@@ -836,7 +837,7 @@ export class DocumentManagerComponent implements OnInit, AfterViewInit {
     if (isItemRename) {
       let updatedNewFileName = this.refrenameForm.renameTxt.value + '.' + this.GetRenameFileExtension;
       if (updatedNewFileName != this.currentRenameItemRecord.name) {
-        this.renameFile(this.currentRenameItemRecord.docId, encodeURIComponent(this.currentRenameItemRecord.name), encodeURIComponent(updatedNewFileName), this.currentRenameItemRecord.referrerId, this.currentRenameItemRecord.docType, this.currentRenameItemRecord.fileBase64);
+        this.renameFile(this.currentRenameItemRecord.docId, encodeURIComponent(this.currentRenameItemRecord.name), encodeURIComponent(updatedNewFileName), this.currentRenameItemRecord.referrerId, this.currentRenameItemRecord.docType, this.currentRenameItemRecord.fileBase64); 
       }
     }
     else {
@@ -870,9 +871,12 @@ export class DocumentManagerComponent implements OnInit, AfterViewInit {
   renameFile(docId: any, OldFileName: any, NewfileName: any, patientId: any, docType: string, fileBase64: any) {
     this.documentmanagerService.renameFile(true, OldFileName, NewfileName, patientId, docId, Number(this.storageService.user.UserId), this.fromPage, null, docType).subscribe((res) => {
       if (res.responseCode == 200) {
+        console.log(this.fileItems);
+        
         let index: number = this.fileItems.map(function (e) { return e.docId; }).indexOf(docId);
         if (index !== -1) {
           this.fileItems[index].name = res.response;
+          this.fileItems[index].filePath = this.fileItems[index].filePath.replace(OldFileName.replace(/%20/g,' ') ,res.response);
           this.fileManager.instance.refresh();
         }
         this.successNotification(res);
