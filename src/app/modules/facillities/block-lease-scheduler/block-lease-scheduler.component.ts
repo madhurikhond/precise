9d12b059b18860a586123a798cc8baf66b2c4824
Maninder <maninder.singh@sensationsolutions.in>
2022-11-17 16:17:44 +0530

    Schedule not Complete(Icon with count) and

--- src/app/modules/facillities/block-lease-scheduler/block-lease-scheduler.component.ts
+++ src/app/modules/facillities/block-lease-scheduler/block-lease-scheduler.component.ts
@@ -52,6 +52,7 @@ export class BlockLeaseSchedulerComponent implements OnInit {
   apiUrl: any;
   fileData: SafeResourceUrl;
   AllBlockLeaseList: any = [];
+  LeasesOfFacilityData: any;
   ngOnInit(): void {
     this.getFacilityParentList();
     this.getModalityList();
@@ -221,12 +222,14 @@ export class BlockLeaseSchedulerComponent implements OnInit {
       }
     });
   }
-  getAllLeasesOfFacilityByStatus(facilityID: any, IsClicked: boolean = true, Status: any = 'PAID') {
+  getAllLeasesOfFacilityByStatus(FacilityID: any, data:any, IsClicked: boolean = true, Status: any = 'PAID') {
     if (IsClicked) {
       this.leasePageNumber = 1;
     }
-    this.facilityID = facilityID;
+    this.LeasesOfFacilityData = data;
+    this.facilityID = FacilityID;
     this.leaseStatus = Status;
+    this.getMonthByDataField(data);
     let body =
     {
       'FacilityId': this.facilityID,
@@ -253,7 +256,7 @@ export class BlockLeaseSchedulerComponent implements OnInit {
   }
   pageChanged(event) {
     this.leasePageNumber = event;
-    this.getAllLeasesOfFacilityByStatus(this.facilityID, false, this.leaseStatus);
+    this.getAllLeasesOfFacilityByStatus(this.facilityID, this.LeasesOfFacilityData, false, this.leaseStatus);
   }
   getLeaseAggrementDetail(path: any,fileData: any) {
     this.apiUrl = `${environment.baseUrl}/v${environment.currentVersion}/`;
@@ -290,6 +293,16 @@ export class BlockLeaseSchedulerComponent implements OnInit {
     }
     this.unSuccessNotification(data);
   }
+  }
+
+  getMonthByDataField(column: any) {
+    debugger
+    let index = 0;
+    if (column.column.caption == 'MRI')
+      index = (Number(Number(column.columnIndex) - 1) / 2);
+    else
+      index = (Number(Number(column.columnIndex) - 2) / 2);
+    this.leaseMonth = column.row.data[`MonthLabels${index}`];
   }
 }
 
