9dd7c76e88a48790f35bcd39b09e0605025a8e36
chirag kamani <tbs.chiragk@gmail.com>
2022-11-18 18:01:59 +0530

    commit for added barcode fonts in ckeditor.

--- src/app/constants/Ckeditor.ts
+++ src/app/constants/Ckeditor.ts
@@ -1,6 +1,10 @@
 export const ckeConfig = {
-    allowedContent: false, 
-    forcePasteAsPlainText: true,
-    removePlugins: 'elementspath,blockquote,preview,save,print,newpage,templates,find,replace,SpellChecker,scayt,flash,smiley,about',
-    removeButtons : 'Checkbox,Radio,Form,TextField,Textarea,Select,Button,ImageButton,HiddenField,PageBreak,SpecialChar,HorizontalRule,SpellChecker, Scayt',   
-  };
+  allowedContent: false,
+  forcePasteAsPlainText: true,
+  removePlugins:
+    'elementspath,blockquote,preview,save,print,newpage,templates,find,replace,SpellChecker,scayt,flash,smiley,about',
+  removeButtons:
+    'Checkbox,Radio,Form,TextField,Textarea,Select,Button,ImageButton,HiddenField,PageBreak,SpecialChar,HorizontalRule,SpellChecker, Scayt',
+  contentsCss: '../../assets/css/customCKEStyles.css',
+  font_names: 'Code128',
+};
