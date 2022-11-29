03b40bf6a940544bc6a00e06552e40e8b7d0ecca
tbs_darshit <tbs.darshit@gmail.com>
2022-11-29 18:04:12 +0530

    commit for R3-1092.

--- src/app/services/lien-portal/lien-portal.service.ts
+++ src/app/services/lien-portal/lien-portal.service.ts
@@ -88,10 +88,29 @@ export class LienPortalService {
   }
 
   AddRadiologistDefaultSign(data: any, showGlobalLoader: boolean = true) {
-    return this._httpService.post('LienPortal/AddRadiologistDefaultSign', data, showGlobalLoader).pipe(
+    return this._httpService.post('LienPortal/AddRadDefaultSign', data, showGlobalLoader).pipe(
       map((res: LienPortalResponse) => res)
     );
   }
+
+  GetFundingCompanyList(data: any, showGlobalLoader: boolean = true) {
+    return this._httpService.post('LienPortal/GetFundingCompanyList', data, showGlobalLoader).pipe(
+      map((res: LienPortalResponse) => res)
+    );
+  }
+
+  GetRadiologistFundingCompanyInfo(data: any, showGlobalLoader: boolean = true) {
+    return this._httpService.post('LienPortal/GetRadiologistFundingCompanyInfo', data, showGlobalLoader).pipe(
+      map((res: LienPortalResponse) => res)
+    );
+  }
+
+  UpsertFundingCompanyInfo(data: any, showGlobalLoader: boolean = true) {
+    return this._httpService.post('LienPortal/UpsertFundingCompanyInfo', data, showGlobalLoader).pipe(
+      map((res: LienPortalResponse) => res)
+    );
+  }
+
 
   successNotification(msg: string) {
     this.notificationService.showNotification({
