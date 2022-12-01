c46ff10ea43081b596b722639e89143bbb0e8271
sureshsensationsolutions <suresh.thakur@sensationsolutions.in>
2022-12-01 17:10:33 +0530

    Mark as paid issue fixed

--- src/app/modules/facillities/facility-management/schd-facilities/pay-invoice-modal/pay-invoice-modal.component.ts
+++ src/app/modules/facillities/facility-management/schd-facilities/pay-invoice-modal/pay-invoice-modal.component.ts
@@ -5,8 +5,9 @@ import * as moment from 'moment';
 import { BlockLeaseSchedulerService } from '../../../../../services/block-lease-scheduler-service/block-lease-scheduler.service';
 import { NotificationService } from '../../../../../services/common/notification.service';
 import { StorageService } from '../../../../../services/common/storage.service';
-import { CommonModule, CurrencyPipe} from '@angular/common';
+import { CommonModule, CurrencyPipe } from '@angular/common';
 import { parse } from 'path';
+import { DateTimeFormatCustom } from 'src/app/constants/dateTimeFormat';
 @Component({
   selector: 'app-pay-invoice-modal',
   templateUrl: './pay-invoice-modal.component.html',
@@ -20,47 +21,44 @@ export class PayInvoiceModalComponent implements OnInit {
   @Input() TotalLeasesAmount: any;
   @Input() facilityId: any;
   ModalResult = ModalResult;
-  payInvoiceForm:FormGroup;
+  payInvoiceForm: FormGroup;
   dobModel: string = '';
   currentDate = new Date();
-  
+
   formattedInputAmount: any;
   inputAmount: any;
-  strAmount: string='';
-  constructor( private currencyPipe : CurrencyPipe,
+  strAmount: string = '';
+  readonly dateTimeFormatCustom = DateTimeFormatCustom;
+  constructor(private currencyPipe: CurrencyPipe,
     private fb: FormBuilder, public modal: NgbActiveModal,
     private readonly blockleasescheduler: BlockLeaseSchedulerService,
     private readonly storageService: StorageService,
-    private readonly notificationService: NotificationService  ) { }
+    private readonly notificationService: NotificationService) { }
 
   ngOnInit(): void {
     this.createPayInvoiceForm();
-    this.formattedInputAmount = this.AmountDetails.TotalAmount;
-   // this.setPayInvoiceForm();
+    this.formattedInputAmount = this.currencyPipe.transform(this.AmountDetails.TotalAmount, '$');
+    // this.setPayInvoiceForm();
   }
-  transformAmount(element){
-    debugger
-    if(this.formattedInputAmount.length>0 && this.formattedInputAmount.slice(0,1) =='$' )
-    {
-   // alert('Amount: ' + this.formattedInputAmount.slice(1,50).toString().replace(/,/g, ""));
-       this.formattedInputAmount = this.formattedInputAmount.slice(1,50).toString().replace(/,/g, "");
-       this.formattedInputAmount = this.currencyPipe.transform(this.formattedInputAmount,'$');
-    element.target.value = this.formattedInputAmount;
+  transformAmount(element) {
+    if (this.formattedInputAmount.length > 0 && this.formattedInputAmount.slice(0, 1) == '$') {
+      this.formattedInputAmount = this.formattedInputAmount.slice(1, 50).toString().replace(/,/g, "");
+      this.formattedInputAmount = this.currencyPipe.transform(this.formattedInputAmount, '$');
+      element.target.value = this.formattedInputAmount;
     }
-   else{ 
-    this.formattedInputAmount = this.currencyPipe.transform(this.formattedInputAmount,'$');
-    element.target.value = this.formattedInputAmount;
-   }
-    
-    
+    else {
+      this.formattedInputAmount = this.currencyPipe.transform(this.formattedInputAmount, '$');
+      element.target.value = this.formattedInputAmount;
+    }
+
+
   }
 
-  createPayInvoiceForm()
-  {
+  createPayInvoiceForm() {
     this.payInvoiceForm = this.fb.group({
-      checkId:['',[Validators.required]],
-      // checkDate:['',[Validators.required]],
-      checkAmount:['',[Validators.required]]
+      checkId: ['', [Validators.required]],
+        checkDate:[this.currentDate,[Validators.required]],
+      checkAmount: ['', [Validators.required]]
     });
   }
   close() {
@@ -70,15 +68,13 @@ export class PayInvoiceModalComponent implements OnInit {
   cancel() {
     this.modal.dismiss(ModalResult.CANCEL);
   }
-  setPayInvoiceForm()
-  {
+  setPayInvoiceForm() {
     // this.payInvoiceForm.patchValue({
     //   checkAmount: new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(this.AmountDetails.TotalAmount)
     // });
   }
-  MakePayment(e)
-  {   
-    var grossAmount = this.payInvoiceFormControls.checkAmount.value.slice(1,50);
+  MakePayment(e) {
+    var grossAmount = this.payInvoiceFormControls.checkAmount.value.replace('$', '')// this.payInvoiceFormControls.checkAmount.value.slice(1,50);
     var data = {
       "FacilityId": this.facilityId,
       "Credits": this.selectedCreditIds,
@@ -89,7 +85,7 @@ export class PayInvoiceModalComponent implements OnInit {
       "TotalCreditsAmount": this.TotalCreditsAmount,
       "TotalLeasesAmount": this.TotalLeasesAmount,
       "UserId": this.storageService.user.UserId,
-      "CheckDate": moment(this.currentDate).format('MM/DD/YYYY hh:mm:ss')
+      "CheckDate": moment(this.payInvoiceFormControls.checkDate.value).format('MM/DD/YYYY hh:mm:ss')
     }
     this.blockleasescheduler.CreatePayments(true, data).subscribe((res) => {
       if (res && res.responseCode == 200) {
