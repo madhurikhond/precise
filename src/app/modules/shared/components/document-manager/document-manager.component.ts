dce3855dd00656c986740bc0c44ab76f4f0311c4
NeerajSensation <Neeraj.kumar@Sensationsolutions.com>
2022-12-06 06:45:39 +0530

    R3B-53 Changes

--- src/app/modules/shared/components/document-manager/document-manager.component.ts
+++ src/app/modules/shared/components/document-manager/document-manager.component.ts
@@ -311,12 +311,13 @@ export class DocumentManagerComponent implements OnInit, AfterViewInit {
         // }
       }
       if (text == 'Download Selected') {
+        debugger
         let fileExtension = this.selectedFileNames.split('.').pop();
         if (this.selectedFileNames.match(/.(jpg|jpeg|png|gif)$/i)) {
                   this.selectedFileBase64String = 'data:image/' + fileExtension + ';base64,' + this.path;
                 }
                 else if (this.selectedFileNames.match(/.(pdf)$/i)) {
-                  this.selectedFileBase64String = 'data:application/pdf;base64,' + this.path;
+                  this.selectedFileBase64String = 'data:application/pdf;base64|' + this.path;
                 }
         this.downloadFile(this.selectedFileNames, this.selectedFileBase64String);
       }
@@ -868,9 +869,9 @@ debugger
     else if (type == 'Download Selected') {
       paths = this.selectedFileItems.map(m => m.filePath);
     }
-    paths.forEach(res => {
-      IdString = paths + "|"
-    })
+    paths.forEach(function (value) {
+      IdString=IdString+"|"+value;
+    }); 
     this.documentmanagerService.getFilesByKeys(true, JSON.stringify(IdString)).subscribe((res) => {
       if (res.response != null) {
         this.downloadAllBasePath = res.response
